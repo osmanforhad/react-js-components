4799c847f566b62cd802891a9b28cb3def5fc5b9
osmanforhad <osmanforhad93@gmail.com>
2022-02-08 22:28:01 +0600

    create spiiner componnet file for spinner component

---
+++ src/components/SpinnerComponent.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class SpinnerComponent extends Component {
+  render() {
+    return (
+      <div>
+        <h4 className="text-center">Spinner Component</h4>
+      </div>
+    );
+  }
+}
+
+export default SpinnerComponent;
